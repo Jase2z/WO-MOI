5949ef7eb9b1cd9f4a7690dc87c16064421017be
Jase2z <Jase2z@users.noreply.github.com>
2022-09-13 04:37:34 -0700

    modified:   log analyse.py

--- log analyse.py
+++ log analyse.py
@@ -4,12 +4,13 @@ from typing import List, Tuple, Type
 from typing_extensions import Self
 from pathlib import Path, PurePath
 from io import SEEK_END
-from asyncio import run, create_task, sleep
 from re import search
 from itertools import islice
 from json import load as jsonload
 from pipe import select, where
-from time import sleep
+from bs4 import BeautifulSoup, SoupStrainer
+from requests import get
+from requests.exceptions import MissingSchema
 from collections import namedtuple
 import logging
 import logging.config
@@ -18,8 +19,9 @@ import yaml
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 DEFAULT_LEVEL = logging.INFO
-def log_setup(log_cfg_path='cfg.yaml'):
+def log_setup(log_cfg_path='log cfg.yaml'):
     if os.path.exists(log_cfg_path):
         with open(log_cfg_path, 'rt') as cfg_file:
             try:
@@ -34,6 +36,17 @@ def log_setup(log_cfg_path='cfg.yaml'):
 
 log_setup()
 logger = logging.getLogger(__name__)
+
+
+def url_validate(url: str) -> get:
+    try:
+        _img = get(url)
+    except MissingSchema:
+        return None
+    except Exception:
+        return None
+    else:
+        return _img
 
 
 @dataclass
@@ -112,110 +125,114 @@ EventStartMsg = namedtuple("EventStartMsg", "datetime action")
 
 class EventLog():
 
-    def __init__(self, log_path: Path, start_datetime: datetime, end_datetime: datetime):
-        self.log_path = log_path
+    def __init__(self, start_datetime: datetime, end_datetime: datetime, html_path: str=None, file_path: Path=None):
+        self.file_path = file_path
         self.tell_pos = None
         self.last_modified = None
-
+        self.web_file = None
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
-        if not isinstance(self.log_path, PurePath):
-            logger.CRITICAL('combat log file path is not a Path object')
+        if html_path != None:
+            self.web_file = url_validate(html_path)
+        elif file_path != None:
+            if not isinstance(self.file_path, PurePath):
+                logger.critical('combat log file path is not a Path object')
+                return
+            if not self.file_path.exists() or not self.file_path.is_file():
+                logger.critical(
+                    'file exists..{},  file is a file..{}, {}'.format(self.file_path.exists(), 
+                    self.file_path.is_file(), str(self.file_path)))
+                return
+        else:
+            logger.error('no valid log data source provided.')
             return
-        if not self.log_path.exists() or not self.log_path.is_file():
-            logger.CRITICAL(
-                'file exists..{},  file is a file..{}, {}'.format(self.log_path.exists(), 
-                self.log_path.is_file(), str(self.log_path)))
-            return
-        self.last_modified = datetime.fromtimestamp(self.log_path.stat().st_mtime)
-        
         # Find line index that has a time stamp which is at most equal to the start date.
         self.start_line_index = 0
         _time = _date = None
-        with open(self.log_path) as f:
-            for line in f:
-                self.start_line_index += 1
-                _date1 = search_datestamp(line)
-                if _date1 != None:
-                    _date = _date1
-                    continue
-                _time1 = search_timestamp(line)
-                if _time1 == None:
-                    logger.CRITICAL('Time does not have valid time.')
-                    return
-                _time = _time1
-                _datetime = datetime.combine(_date, _time)
-                if _datetime < self.start_datetime:
-                    self.last_datetime = _datetime
-                if _datetime == self.start_datetime:
-                    self.start_line_index -= 1
-                    break
-                elif _datetime > self.start_datetime:
-                    self.start_line_index -= 1
-                    break
+        self._reset_line_gen()
+        for line in self.line_gen:
+            self.start_line_index += 1
+            _date1 = search_datestamp(line)
+            if _date1 != None:
+                _date = _date1
+                continue
+            _time1 = search_timestamp(line)
+            if _time1 == None:
+                logger.critical('Time does not have valid time.')
+                return
+            _time = _time1
+            _datetime = datetime.combine(_date, _time)
+            if _datetime < self.start_datetime:
+                self.last_datetime = _datetime
+            if _datetime == self.start_datetime:
+                self.start_line_index -= 1
+                break
+            elif _datetime > self.start_datetime:
+                self.start_line_index -= 1
+                break
 
-    def log_changes(self):
-        while self.end_datetime > self.last_datetime:
-            self.process_lines()
+    def _reset_line_gen(self) -> None:
+        if self.web_file != None:
+            self.line_gen = (line for line in self.web_file.text.split(f'\r\n'))
+        elif self.file_path != None:
+            self.line_gen = (line for line in open(self.file_path, "r"))
+        else:
+            logger.critical('no valid log data source provided.')
+            return
 
-    async def updating_log_changes(self):
-        while self.end_datetime > self.last_datetime:
-            while datetime.fromtimestamp(self.log_path.stat().st_mtime) == self.last_modified:
-                await sleep(0.1)
-            self.process_lines()
 
     def process_lines(self):
         _time = self.last_datetime.time()
         _date = self.last_datetime.date()
         _datetime = datetime.combine(_date, _time)
         esm = None
-        with open(self.log_path) as f:
-            line_cnt = 0
-            for line in islice(f, self.start_line_index, None):
-                # Get time and date from lines in log.
-                # The date and time identifiers are on different lines. And date only appears on log in.
-                # Logging started 2022-09-01
-                # [00:08:05] The shards doesn't fit.
-                line_cnt += 1
-                _date1 = search_datestamp(line)
-                if _date1 != None:
-                    _date = _date1
-                    continue
-                _time1 = search_timestamp(line)
-                if _time1 == None:
-                    # With the exception of line, "Logging started", their should always be a time stamp.
-                    logger.error(
-                        f'Last datetime, {_datetime}. Invalid time stamp in, "{line.strip()}". line # {self.start_line_index + line_cnt}')
-                    return
-                _time = _time1
+        line_cnt = 0
+        self._reset_line_gen()
+        for line in islice(self.line_gen, self.start_line_index, None):
+            # Get time and date from lines in log.
+            # The date and time identifiers are on different lines. And date only appears on log in.
+            # Logging started 2022-09-01
+            # [00:08:05] The shards doesn't fit.
+            line_cnt += 1
+            _date1 = search_datestamp(line)
+            if _date1 != None:
+                _date = _date1
+                continue
+            _time1 = search_timestamp(line)
+            if _time1 == None:
+                # With the exception of line, "Logging started", their should always be a time stamp.
+                logger.error(
+                    f'Last datetime, {_datetime}. Invalid time stamp in, "{line.strip()}". line # {self.start_line_index + line_cnt}')
+                return
+            _time = _time1
+            _datetime1 = datetime.combine(_date, _time)
+            if _datetime1 < _datetime:
+                # Wurm doesn't have date identifying lines for when time hits 24h marker.
+                _date += timedelta(days=1)
                 _datetime1 = datetime.combine(_date, _time)
-                if _datetime1 < _datetime:
-                    # Wurm doesn't have date identifying lines for when time hits 24h marker.
-                    _date += timedelta(days=1)
-                    _datetime1 = datetime.combine(_date, _time)
-                else:
-                    _datetime = _datetime1
-                if self.end_datetime < _datetime:
-                    break
-                #  Identify and classify the line.
-                sm = list(start_messages | where(lambda x:x.line == line[11:].strip()))
-                if len(sm) > 0 and esm != None:
-                    logger.warning(
-                        f'Last datetime, {_datetime}. Second start message in, "{line.strip()}". line # {self.start_line_index + line_cnt}')
-                    esm = None
-                    continue
-                if len(sm) > 0:
-                    esm = EventStartMsg(_datetime, sm[0].action)
-                    continue
-                em = list(end_messages | where(lambda x:x.line == line[11:].strip()))
-                if len(em) > 0 and esm == None:
-                    logger.warning(
-                        f'Last datetime, {_datetime}. End message without start message, "{line.strip()}". line # {self.start_line_index + line_cnt}')
-                    esm = None
-                    continue
-                if len(em) > 0:
-                    action_attempts.append(ActionAttempt(esm.datetime, _datetime, em[0].action))
-                    esm = None
+            else:
+                _datetime = _datetime1
+            if self.end_datetime < _datetime:
+                break
+            #  Identify and classify the line.
+            sm = list(start_messages | where(lambda x:x.line == line[11:].strip()))
+            if len(sm) > 0 and esm != None:
+                logger.warning(
+                    f'Last datetime, {_datetime}. Second start message in, "{line.strip()}". line # {self.start_line_index + line_cnt}')
+                esm = None
+                continue
+            if len(sm) > 0:
+                esm = EventStartMsg(_datetime, sm[0].action)
+                continue
+            em = list(end_messages | where(lambda x:x.line == line[11:].strip()))
+            if len(em) > 0 and esm == None:
+                logger.warning(
+                    f'Last datetime, {_datetime}. End message without start message, "{line.strip()}". line # {self.start_line_index + line_cnt}')
+                esm = None
+                continue
+            if len(em) > 0:
+                action_attempts.append(ActionAttempt(esm.datetime, _datetime, em[0].action))
+                esm = None
     
     def get_out_of_window(self, window_time: int) -> list:
         for ix in range(1, len(action_attempts)):
@@ -233,15 +250,20 @@ class EventLog():
                    
 
 if __name__ == "__main__":
-    el = EventLog(Path 
-        (r'C:\Users\Jason\AppData\Local\Programs\Wurm Online\players\joedobo\logs\_Event.2022-09.txt'),
-            datetime.fromisoformat('2022-09-02T18:42:00'),
-            datetime.fromisoformat('2022-09-03T00:56:00'))
+    embed = '<iframe src="https://onedrive.live.com/embed?cid=8BF93030BCC12554&resid=8BF93030BCC12554%211003&authkey=AFGGQYkSItv2XGM" width="98" height="120" frameborder="0" scrolling="no"></iframe>'
+    h_tag = BeautifulSoup(embed, "html5lib")
+    web = h_tag.iframe["src"].replace("embed", "download", 1)
+    #https://1drv.ms/t/s!AlQlwbwwMPmLh2swsZEmt9SkmupZ?e=ttI16P
+    #https://cdn.matix-media.net/dd/36be025d
+
+    fil = Path(r'C:\Users\Jason\AppData\Local\Programs\Wurm Online\players\joedobo\logs\_Event.2022-09.txt')
+    el = EventLog(datetime.fromisoformat('2022-09-02T18:42:00'),
+                datetime.fromisoformat('2022-09-03T00:56:00'), file_path=fil)
     el.process_lines()
     act_dur = el.get_action_durations()
     stag_dur = el.get_stagger_durations()
 
-    #TODO The duration lists aren't helpful explaining why a data pint is what it is. 
+    #TODO The duration lists aren't helpful explaining why a data point is what it is. 
     # I need a way to go look at outrageous data points and see what happened.
     # Maybe a named tuple with the data point and a indices references for its
     # cause from action_attempts.
